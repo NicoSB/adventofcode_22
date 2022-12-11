cc7a23288a5e97b8fea3d85f2fe30baca4b8049b
Nico Strebel <nico.strebel@acodis.io>
2022-12-11 14:33:19 +0100

    Day8

--- src/Day8/Part1.hs
+++ src/Day8/Part1.hs
@@ -7,14 +7,13 @@ import System.IO
 import qualified Data.Binary.Builder as Text
 import Debug.Trace (traceShow, trace)
 import Data.Set (Set, insert, empty, fromList)
-import GHC.Tc.Gen.Head (isVisibleArg)
 
 main :: IO ()
 main = do
   args <- getArgs
   lines <- do getLines (head args)
   let field = getVisibles $ parseField lines
-  print field
+  -- print field
   print $ length $ filter id (concat field)
 getLines :: String -> IO [String]
 getLines filePath = do
@@ -27,7 +26,7 @@ parseField = map (map (\c -> read [c] :: Int))
 getVisibles :: [[Int]] -> [[Bool]]
 getVisibles field =
   let width = length (head field)
-      height = length (traceShow field field)
+      height = length field
   in map (\i -> map (\j -> isVisible i j height width field) (take width [0..])) (take height [0..])
 
 isVisible :: Int -> Int -> Int -> Int -> [[Int]] -> Bool
@@ -35,8 +34,8 @@ isVisible i j height width field =
   let value = at i j field
       lefts = map (\v -> at i v field) (take (j) [0..])
       ups = map (\v -> at v j field) (take (i) [0..])
-      rights = map (\v -> at i v field) (take (width - j - 1) [(i - 1)..])
-      downs = map (\v -> at v j field) (take (height - i - 1) [(i - 1)..])
+      rights = map (\v -> at i v field) (take (width - j - 1) [(j + 1)..])
+      downs = map (\v -> at v j field) (take (height - i - 1) [(i + 1)..])
       canSeeFromLeft = isTallest value lefts
       canSeeFromUp = isTallest value ups
       canSeeFromRight = isTallest value rights
@@ -46,10 +45,9 @@ isVisible i j height width field =
 
 isTallest :: Int -> [Int] -> Bool
 isTallest value others =
-    not $ any (>= value) (trace ((show value) ++ show others) others)
+    not $ any (>= value) others
 
 at :: Int -> Int -> [[Int]] -> Int
 at i j field =
   (field !! i) !! j
 
-  
